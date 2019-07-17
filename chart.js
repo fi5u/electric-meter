8c651242e13b05d13bae42859daf25771d7020b4
Tommy Fisher <tommybfisher@gmail.com>
2019-07-17 15:14:12 +0300

    Show day/month axis

--- chart.js
+++ chart.js
@@ -1,5 +1,7 @@
 const ctx = document.getElementById('chart')
 
+const dayAxis = ['0:00', '1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:00', '8:00', '9:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00']
+
 /**
  * Set new date on chart
  * @param {Array} data Data to output
@@ -31,14 +33,15 @@ function setChartDate(data, date, timeframe) {
         })
     }
 
-    outputChart(data)
+    outputChart(data, timeframe)
 }
 
 /**
  * Output a chart
  * @param {Array} data Data to output
+ * @param {'day' | 'month'} timeframe Current timeframe
  */
-function outputChart(data) {
+function outputChart(data, timeframe) {
     new Chart(ctx, {
         data: {
             datasets: [{
@@ -46,7 +49,7 @@ function outputChart(data) {
                 data: data,
                 label: 'Energy usage (kW)'
             }],
-            labels: ['0:00', '1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:00', '8:00', '9:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00']
+            labels: timeframe === 'day' ? dayAxis : data.map((d, i) => i + 1)
         },
         options: {
             scales: {
